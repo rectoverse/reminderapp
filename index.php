213e5b83af9d2162b673ca2836c332817deaceb8
Arjun Krishna <arjunxnujra@gmail.com>
2020-03-31 23:44:18 +0530

    Update index.php

--- index.php
+++ index.php
@@ -5,7 +5,7 @@ $password = "";
 $dbname = "internship";
 
 // Create connection
-$conn = new mysqli($servername, $username, $password, $dbname);
+$conn = new mysqli($servername, $username, $password, $dbname);#just another comment
 // Check connection
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
